ddc3cfcb764f7fa3768577d7880e6b78bcfe247d
Eoin <eoinom@gmail.com>
2019-09-21 00:45:30 +0100

    Amended page-query to CMS for Roots & Youth

--- src/pages/RootsAndYouth.vue
+++ src/pages/RootsAndYouth.vue
@@ -2,15 +2,15 @@
   <Layout>    
    
     <br>
-    <h2>Roots and Youth</h2>
-    <div v-for="edge in $page.rootsVideos.edges" :key="edge.node.id">
+    <!-- <h2>Roots and Youth</h2> -->
+    <h2>{{ $page.RootsAndYouth.edges[0].node.pageTitle }}</h2>
+    <div v-for="edge in $page.RootsAndYouth.edges" :key="edge.node.id">
       <h1>{{ edge.node.title }}</h1>
-      <br>Length: {{ edge.node.length }}
-      <br>Caption: {{ edge.node.caption }}
+
       <div style="padding:56.25% 0 0 0;position:relative;">
         <iframe 
-          :src="edge.node.url + '?autoplay=0&color=505050&title=0&byline=0&portrait=0'"
-          style="position:absolute;top:0;left:0;width:100%;height:100%;" 
+          :src="edge.node.fullVideo.url + '?autoplay=0&color=505050&title=0&byline=0&portrait=0'"
+          style="position:absolute;top:0;left:0;width:50%;height:50%;" 
           frameborder="0" 
           webkitallowfullscreen mozallowfullscreen allowfullscreen>
         </iframe>
@@ -24,34 +24,56 @@
 
 <page-query>
 {
-  rootsVideos: allRootsVideo(sortBy: "title", order: ASC) {
-    totalCount
+  RootsAndYouth: allRootsAndYouth {
     edges {
       node {
         id
-        title
-        url
-        length
-        caption
+        pageTitle
+        headingImg
+        content
+        slides {
+          orderNo
+          img
+        }
+        slideDuration
+        videos {
+          title
+          subText
+          url
+          orderNo
+          duration
+        }
+        fullVideo {
+          title
+          text
+          url
+          duration
+        }
       }
     }
-  }
+  }	
 }
 </page-query>
 
 
 <script>
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
+  metaInfo() {    // https://github.com/gridsome/gridsome/issues/306 (How do you use the queried GraphQL data in the <script>?)
+    return {
+      title: this.$page.RootsAndYouth.edges[0].node.pageTitle,  // <-- "this" is the Vue instance with $page
+    }
+  },
 }
 </script>
 
 
 
-<style>
-.home-links a {
+<style scoped>
+/* .home-links a {
   margin-right: 1rem;
+} */
+
+body {
+  background-color: #dddddd
 }
 </style>
