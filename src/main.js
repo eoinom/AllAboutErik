518394bf0d7a3a7a06f9e7c80107530b72159b3d
Eoin <eoinom@gmail.com>
2019-09-20 02:14:11 +0100

    Add 'Play' font-awesome icon

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@
 import DefaultLayout from '~/layouts/Default.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { faVolumeUp, faVolumeMute } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeUp, faVolumeMute, faPlay } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 config.autoAddCss = false;
-library.add(faVolumeUp, faVolumeMute)
+library.add(faVolumeUp, faVolumeMute, faPlay)
 
 export default function (Vue, { router, head, isClient, appOptions }) {
   // for getting page leave transitions to work (https://github.com/gridsome/gridsome/issues/24)
