54e27bf6c58e78d94c3e4539ad6c3c6b12f076eb
Eoin <eoinom@gmail.com>
2019-09-24 01:51:51 +0100

    WIP on Roots & Youth page

---
+++ src/components/SlideshowImages2.vue
@@ -0,0 +1,103 @@
+<template>
+<div>
+    <b-carousel
+      id="carousel-roots&youth"
+      v-model="slide"
+      :interval="4000"
+      fade
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333; "
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd" 
+    >
+      <b-carousel-slide
+        v-for="(image, iImg) in images"
+        :key="iImg"
+        caption=""
+        text=""
+        :img-src="image" >
+      </b-carousel-slide>
+    </b-carousel>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'SlideshowImages2',
+
+  props: {
+    slides: {
+      default: () => [],
+      type: Array,
+    },
+  },
+
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    };
+  },
+
+  computed: {
+    images() {    
+      return this.slides.map(a => a.img)
+    },
+    currentImg() {
+      return this.images[this.slide]
+    }
+  },
+
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    }
+  },
+
+  mounted() {
+  }
+};
+</script>
+
+
+<style>
+
+@font-face {
+  font-family: NeueHaasGroteskText Pro5;
+  src: url('../assets/fonts/nhaasgrotesktxpro-55rg.eot'); /* IE9 Compat Modes */
+  src: url('../assets/fonts/nhaasgrotesktxpro-55rg.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('../assets/fonts/nhaasgrotesktxpro-55rg.woff') format('woff'), /* Pretty Modern Browsers */
+       url('../assets/fonts/nhaasgrotesktxpro-55rg.svg#NHaasGroteskTXPro-55Rg') format('svg'); /* Legacy iOS */
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: NeueHaasGroteskText Pro6;
+  src: url('../assets/fonts/nhaasgrotesktxpro-65md.eot'); /* IE9 Compat Modes */
+  src: url('../assets/fonts/nhaasgrotesktxpro-65md.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('../assets/fonts/nhaasgrotesktxpro-65md.woff') format('woff'), /* Pretty Modern Browsers */
+       url('../assets/fonts/nhaasgrotesktxpro-65md.svg#NHaasGroteskTXPro-55Rg') format('svg'); /* Legacy iOS */
+  font-weight: normal;
+}
+
+.carousel-caption > p {
+  color: #FFFFFF;
+  /* font-size: 30px; */
+  font-size: 1.875rem;
+  font-family: 'NeueHaasGroteskText Pro6';
+  font-feature-settings: 'liga';
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 2px 2px 5px rgba(0,0,0,0.65);
+  line-height: 43px;
+  letter-spacing: 2px;    
+}
+
+</style>
--- src/pages/RootsAndYouth.vue
+++ src/pages/RootsAndYouth.vue
@@ -1,10 +1,57 @@
 <template>
-  <Layout>    
+  <Layout> 
+
+    <b-container fluid class="main-col">
+      <!-- Content here -->
+      <b-row no-gutters style="max-height:1224px; width:auto;">
+        <b-col style="background-color:orange; max-height:1224px; width:auto;">
+          
+          <div class="slideshow">
+            <slideshow-images :slides="slides" />
+          </div>
+
+          <!-- <g-image alt="All About Erik logo" v-if="titleImg != null" :src="titleImg" class="titleImg"/>
+          <span v-html="mainText" class="mainText" /> -->
+          
+          <b-container fluid class="slideshowOverlay">
+            <b-row>
+              <b-col class="mainContent">
+                <g-image alt="All About Erik logo" v-if="titleImg != null" :src="titleImg" class="titleImg"/>
+                <span v-html="mainText" class="mainText" />
+              </b-col>
+            </b-row>
+          </b-container>
+
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col cols="12" md="6" style="background-color:cyan">
+          Some text here
+        </b-col>
+        <b-col cols="12" md="6" style="background-color:limegreen">
+          Some text here
+        </b-col>        
+        <b-col cols="12" md="6" style="background-color:cyan">
+          Some text here
+        </b-col>
+        <b-col cols="12" md="6" style="background-color:limegreen">
+          Some text here
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col style="background-color:orange">
+          Some text here
+        </b-col>
+      </b-row>
+
+    </b-container>   
    
-    <br>
+    <!-- <br> -->
     <!-- <h2>Roots and Youth</h2> -->
-    <h2>{{ $page.RootsAndYouth.edges[0].node.pageTitle }}</h2>
-    <div v-for="edge in $page.RootsAndYouth.edges" :key="edge.node.id">
+    <!-- <h2>{{ $page.RootsAndYouth.edges[0].node.pageTitle }}</h2> -->
+    <!-- <div v-for="edge in $page.RootsAndYouth.edges" :key="edge.node.id">
       <h1>{{ edge.node.title }}</h1>
 
       <div style="padding:56.25% 0 0 0;position:relative;">
@@ -17,7 +64,7 @@
       </div>
       
       <hr>
-    </div>
+    </div> -->
 
   </Layout>
 </template>
@@ -56,12 +103,35 @@
 </page-query>
 
 
-<script>
+<script scoped>
+import SlideshowImages from '../components/SlideshowImages2.vue'
+
 export default {
-  metaInfo() {    // https://github.com/gridsome/gridsome/issues/306 (How do you use the queried GraphQL data in the <script>?)
+  metaInfo() {
     return {
       title: this.$page.RootsAndYouth.edges[0].node.pageTitle,  // <-- "this" is the Vue instance with $page
     }
+  },
+
+  data() {
+    return {      
+    }
+  },
+
+  computed: {
+    titleImg() {
+      return this.$page.RootsAndYouth.edges[0].node.headingImg
+    },
+    slides() {
+      return this.$page.RootsAndYouth.edges[0].node.slides
+    },  
+    mainText(){
+      return this.$page.RootsAndYouth.edges[0].node.content
+    },
+  },
+
+  components: {
+    'slideshow-images':     require('../components/SlideshowImages2.vue').default,
   },
 }
 </script>
@@ -69,11 +139,97 @@ export default {
 
 
 <style scoped>
-/* .home-links a {
-  margin-right: 1rem;
-} */
+@font-face {
+  font-family: NeueHaasGroteskText Pro6;
+  src: url('../assets/fonts/nhaasgrotesktxpro-65md.eot'); /* IE9 Compat Modes */
+  src: url('../assets/fonts/nhaasgrotesktxpro-65md.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('../assets/fonts/nhaasgrotesktxpro-65md.woff') format('woff'), /* Pretty Modern Browsers */
+       url('../assets/fonts/nhaasgrotesktxpro-65md.svg#NHaasGroteskTXPro-55Rg') format('svg'); /* Legacy iOS */
+  font-weight: normal;
+}
 
 body {
   background-color: #dddddd
 }
+
+.main-col {
+  padding: 0 6.5%; 
+  
+}
+
+.slideshowOverlay {
+  z-index: 100;
+  height: 100%;
+  top: 0;
+  position: absolute;
+}
+
+.slideshowOverlay .mainContent{
+  /* position: relative; */
+  /* position: absolute; */
+  /* top: -423px; */
+  /* top: 60%; */
+  z-index: 500;
+  /* height: 0; */
+  width: 100%;
+  /* margin: 0 18%; */
+  text-align: center;
+  padding-top: 30%;
+  padding-left: 19%;
+  padding-right: 19%;
+}
+
+.titleImg {
+  /* display: inline-block; */
+  width: 100%;
+  max-width: 1492px;
+  height: auto;
+  /* position: relative; */
+  margin: auto;
+
+  /* position: absolute;
+  top: 223px;
+  z-index: 500; */
+}
+
+.mainText {
+  color: #FFFFFF;
+  /* font-size: 30px; */
+  font-size: 1.85rem;
+  font-family: 'NeueHaasGroteskText Pro6';
+  font-feature-settings: 'liga';
+  font-weight: 500;
+  /* text-align: center; */
+  text-shadow: 2px 2px 5px rgba(0,0,0,0.65);
+  line-height: 43px;
+  letter-spacing: 2px;    
+}
+
+
+@media (min-width : 768px) and (max-width : 1024px) {
+  .slideshowOverlay .mainContent{
+    padding-top: 25%;
+    padding-left: 15%;
+    padding-right: 15%;
+  }
+
+  .mainText {
+    font-size: 1.25rem;
+    line-height: 30px;
+  }
+}
+
+@media (max-width : 767px) {
+  .slideshowOverlay .mainContent{
+    padding-top: 20%;
+    padding-left: 10%;
+    padding-right: 10%;
+  }
+
+  .mainText {
+    font-size: 1.0rem;
+    line-height: 20px;
+  }
+}
+
 </style>
