040d8b42fb449be28f0698a50fb641360e1531e6
Eoin <eoinom@gmail.com>
2019-09-25 01:53:44 +0100

    WIP on Roots & Youth page

--- src/pages/RootsAndYouth.vue
+++ src/pages/RootsAndYouth.vue
@@ -2,22 +2,23 @@
   <Layout> 
 
     <b-container fluid class="main-col">
-      <!-- Content here -->
-      <b-row no-gutters style="max-height:1224px; width:auto;">
+      
+      <b-row no-gutters style="max-height:1224px; width:auto;" class="mb-1 px-1">
         <b-col style="background-color:orange; max-height:1224px; width:auto;">
           
           <div class="slideshow">
             <slideshow-images :slides="slides" />
           </div>
-
-          <!-- <g-image alt="All About Erik logo" v-if="titleImg != null" :src="titleImg" class="titleImg"/>
-          <span v-html="mainText" class="mainText" /> -->
           
           <b-container fluid class="slideshowOverlay">
             <b-row>
               <b-col class="mainContent">
                 <g-image alt="All About Erik logo" v-if="titleImg != null" :src="titleImg" class="titleImg"/>
                 <span v-html="mainText" class="mainText" />
+                <br />
+                <g-image alt="Scroll text" v-if="titleImg != null" src="~/assets/images/scroll.png" class=""/>
+                <br />
+                <g-image alt="All About Erik logo" v-if="titleImg != null" src="~/assets/images/arrow-full-down.png" class=""/>
               </b-col>
             </b-row>
           </b-container>
@@ -25,22 +26,21 @@
         </b-col>
       </b-row>
 
-      <b-row>
-        <b-col cols="12" md="6" style="background-color:cyan">
-          Some text here
-        </b-col>
-        <b-col cols="12" md="6" style="background-color:limegreen">
-          Some text here
-        </b-col>        
-        <b-col cols="12" md="6" style="background-color:cyan">
-          Some text here
-        </b-col>
-        <b-col cols="12" md="6" style="background-color:limegreen">
-          Some text here
+      <b-row no-gutters class="mb-1">
+        <b-col cols="12" lg="6" v-for="video in videos" :key="video.title" class="my-1 px-1">
+          <!-- <div style="padding:56.25% 0 0 0;position:relative;z-index:900">
+            <iframe 
+              :src="video.url + '?autoplay=0&color=505050&title=0&byline=0&portrait=0'"
+              style="position:absolute;top:0;left:0;width:1000px;height:500px;" 
+              frameborder="0" 
+              webkitallowfullscreen mozallowfullscreen allowfullscreen>
+            </iframe>
+          </div> -->
+          <g-image :alt="video.title" v-if="video.thumbnailImg != null" :src="video.thumbnailImg" class="thumbnailImg"/>
         </b-col>
       </b-row>
 
-      <b-row>
+      <b-row no-gutters class="pb-2 px-1">
         <b-col style="background-color:orange">
           Some text here
         </b-col>
@@ -87,6 +87,7 @@
           title
           subText
           url
+          thumbnailImg
           orderNo
           duration
         }
@@ -106,10 +107,10 @@
 <script scoped>
 import SlideshowImages from '../components/SlideshowImages2.vue'
 
-export default {
+export default { 
   metaInfo() {
     return {
-      title: this.$page.RootsAndYouth.edges[0].node.pageTitle,  // <-- "this" is the Vue instance with $page
+      title: this.$page.RootsAndYouth.edges[0].node.pageTitle
     }
   },
 
@@ -128,6 +129,15 @@ export default {
     mainText(){
       return this.$page.RootsAndYouth.edges[0].node.content
     },
+    videos() {
+      return this.$page.RootsAndYouth.edges[0].node.videos
+    }
+  },
+
+  methods: {
+  },
+
+  mounted() {
   },
 
   components: {
@@ -148,12 +158,16 @@ export default {
   font-weight: normal;
 }
 
-body {
+.layout {
   background-color: #dddddd
 }
 
+.row {
+  margin-bottom: 20px;
+}
+
 .main-col {
-  padding: 0 6.5%; 
+  padding: 0 6%; 
   
 }
 
@@ -174,7 +188,7 @@ body {
   width: 100%;
   /* margin: 0 18%; */
   text-align: center;
-  padding-top: 30%;
+  padding-top: 32%;
   padding-left: 19%;
   padding-right: 19%;
 }
@@ -203,6 +217,12 @@ body {
   text-shadow: 2px 2px 5px rgba(0,0,0,0.65);
   line-height: 43px;
   letter-spacing: 2px;    
+}
+
+.thumbnailImg {
+  /* width: 1092px; */
+  width: 100%;
+  height: auto;
 }
 
 
