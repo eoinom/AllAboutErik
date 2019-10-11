02aa22437cdfc4ffeefd2b3bf0f6c641a43c2f39
Eoin <eoinom@gmail.com>
2019-10-11 01:49:06 +0100

    Add component template

---
+++ src/components/_component-template.vue
@@ -0,0 +1,38 @@
+<template>
+
+</template>
+
+
+<script scoped>
+export default { 
+  name: '',
+
+  props: {
+    
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  },
+
+  mounted() {
+  },
+
+  components: {
+  },
+}
+</script>
+
+
+
+<style scoped>
+
+
+</style>
