ad80aff619a4373c0e176757c9e1293c1d8862f2
Eoin O'Malley <eoinom@gmail.com>
2019-10-15 21:11:02 +0100

    Create MusicalFriends page from template and add page query for data from CMS

---
+++ src/pages/Musical Journey/MusicalFriends.vue
@@ -0,0 +1,93 @@
+<template>
+  <Layout> 
+    <b-container fluid class="main-col">
+
+    </b-container>
+  </Layout>
+</template>
+
+<page-query>
+{
+  MusicalFriends: allMusicalFriends {
+    edges {
+      node {
+        id
+        pageTitle
+        backgroundImg
+        titleImg
+        content
+        friends {
+          name
+          text
+          link
+          orderNo
+          thumbnailImg
+          imgOrientation
+          imgPosition
+        }
+      }
+    }
+  }	
+}
+</page-query>
+
+
+<script scoped>
+
+export default { 
+  metaInfo() {
+    return {
+      title: this.$page.MusicalFriends.edges[0].node.pageTitle
+    }
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+    titleImg() {
+      return this.$page.MusicalFriends.edges[0].node.titleImg
+    }
+  },
+
+  methods: {
+  },
+
+  mounted() {
+  },
+
+  components: {
+  },
+}
+</script>
+
+
+
+<style scoped>
+
+
+/* Responsive breakpoints ref: https://getbootstrap.com/docs/4.3/layout/overview/ */
+
+/* Extra small devices (portrait phones, less than 576px) */
+@media (max-width: 575.98px) {
+
+}
+
+/* Small devices (landscape phones, 576px and up) */
+@media (min-width: 576px) and (max-width: 767.98px) {
+
+}
+
+/* Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) and (max-width: 991.98px) {
+
+}
+
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) and (max-width: 1199.98px) { 
+
+}
+
+</style>
