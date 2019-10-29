ec7df2d07428ebd38dcfabdab68a46c9bbca311e
Eoin O'Malley <eoinom@gmail.com>
2019-10-29 22:13:16 +0000

    Remove old unused code.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,8 +3,7 @@
 
     <b-container fluid>
       <div class="soundIconContainer">
-        <span class="icon" v-b-tooltip.hover="{ variant: 'light' }" :title="tooltipText">
-          <!-- <font-awesome :icon="audioFontAwesomeIcon" @click="clickAudioIcon()" /> -->
+        <span v-b-tooltip.hover="{ variant: 'light' }" :title="tooltipText">
           <g-image v-if="audioPlaying && !audioMuted" alt="Mute background music" src="~/assets/images/sound-playing.png" class="audioIcon" @click="clickAudioIcon()" />
           <g-image v-if="!audioPlaying || audioMuted" alt="Play background music" src="~/assets/images/sound-muted.png" class="audioIcon" @click="clickAudioIcon()" />          
         </span>
@@ -81,8 +80,6 @@ export default {
 
   data() {
     return {
-      windowWidth: 0,
-      windowHeight: 0,
       audioPlaying: false,
       audioMuted: true,
       audioFinished: false,
@@ -120,12 +117,6 @@ export default {
     audioFadeOutDuration() {
       return this.$page.HomePage.edges[0].node.bgAudioFadeOutDuration
     },
-    // audioFontAwesomeIcon() {
-    //   if (!this.audioPlaying)
-    //     return ['fas', 'play']
-    //   else
-    //     return this.audioMuted ? ['fas', 'volume-mute'] : ['fas', 'volume-up']
-    // },
     tooltipText() {
       if (this.audioPlaying && !this.audioMuted) {
         return 'Mute background music'
@@ -226,13 +217,6 @@ export default {
   },
 
   mounted() {
-    this.$nextTick(() => {
-      window.addEventListener('resize', () => {
-        this.windowWidth = window.innerWidth
-        this.windowHeight = window.innerHeight
-      });
-    })
-
     this.playAndFadeAudio()
   },
   beforeDestroy() {  
@@ -247,82 +231,12 @@ export default {
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap');
 
-
-$items: 14;
-$animation-time: 5s;
-$transition-time: 3s;
-$scale: 10%;
-
-$total-time: ($animation-time * $items);
-$scale-base-1: (1 + $scale / 100%);
-
 .slideshow {
   position: absolute;
   width: 100vw;
   height: 100vh;
   overflow: hidden;
 }
-
-.slideshow-image {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: no-repeat 50% 50%;
-  background-size: cover;
-  animation-name: kenburns;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-  animation-duration: $total-time;
-  transform-origin: top left;
-  opacity: 0.6;
-  transform: scale($scale-base-1);
-
-  @for $i from 1 through $items {
-    &:nth-child(#{$i}) {
-      animation-name: kenburns-#{$i};
-      z-index: ($items - $i);
-    }
-  }
-}
-
-@for $i from 1 through $items {
-  @keyframes kenburns-#{$i} {
-    $animation-time-percent: percentage($animation-time / $total-time);
-    $transition-time-percent: percentage($transition-time / $total-time);
-
-    $t1: ($animation-time-percent * ($i - 1) - $transition-time-percent / 2);
-    $t2: ($animation-time-percent * ($i - 1) + $transition-time-percent / 2);
-    @if($t1 < 0%) { $t1: 0%; }
-    @if($t2 < 0%) { $t2: 0%; }
-
-    $t3: ($animation-time-percent * ($i) - $transition-time-percent / 2);
-    $t4: ($animation-time-percent * ($i) + $transition-time-percent / 2);
-    @if($t3 > 100%) { $t3: 100%; }
-    @if($t4 > 100%) { $t4: 100%; }
-
-    $t5: (100% - $transition-time-percent / 2);
-    $t6: (($t4 - $t1) * 100% / $t5);
-
-    #{$t1} { opacity: 1; transform: scale($scale-base-1); }
-    #{$t2} { opacity: 1; }
-    #{$t3} { opacity: 1; }
-    #{$t4} { opacity: 0; transform: scale(1); }
-
-    @if($i != $items) {
-      100% { opacity: 0; transform: scale($scale-base-1); }
-    }
-
-    @if($i == 1) {
-      $scale-plus: ($scale * (100% - $t5) / $t4);
-      $scale-plus-base-1: (1 + ($scale + $scale-plus) / 100%);
-
-      #{$t5} { opacity: 0; transform: scale($scale-plus-base-1); }
-      100% { opacity: 1; }
-    }
-  }
-}
-
-
 
 .layout {
   padding: 0
