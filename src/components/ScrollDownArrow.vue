09a1b2e966179638c3d37d322fc303ae13564d10
Eoin <eoinom@gmail.com>
2020-06-03 00:22:10 +0100

    Amend responsive sizes for scroll down arrow

--- src/components/ScrollDownArrow.vue
+++ src/components/ScrollDownArrow.vue
@@ -40,15 +40,48 @@ export default {
 }
 
 #scrollImg {
-  width: 11.5%;
+  width: 11.5vw;
   max-width: 157px;
   min-width: 78.5px;
 }
 
 #scrollArrowImg {
-  width: 2.8%;
+  width: 2.8vw;
   max-width: 38px;
   min-width: 19px;
+}
+
+
+/* Responsive breakpoints ref: https://getbootstrap.com/docs/4.3/layout/overview/ */
+
+/* Extra small devices (portrait phones, less than 576px) */
+@media (max-width: 575.98px) {
+  #scrollImg {
+    width: 20vw;
+  }
+  #scrollArrowImg {
+    width: 5vw;
+  }
+}
+
+/* Small devices (landscape phones, 576px and up) */
+@media (min-width: 576px) and (max-width: 767.98px) {
+  #scrollImg {
+    width: 15vw;
+  }
+  #scrollArrowImg {
+    width: 4vw;
+  }
+}
+
+/* Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) and (max-width: 991.98px) {
+
+}
+
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) and (max-width: 1199.98px) {
+
 }
 
 </style>
