4e3a410057e4750376598b8eb1a248f859acf267
Eoin <eoinom@gmail.com>
2020-09-01 02:13:38 +0100

    Turn off nav dots on narrative archives

--- src/templates/ArchiveNarrative.vue
+++ src/templates/ArchiveNarrative.vue
@@ -290,7 +290,7 @@ export default {
         duration: 850,
         easing: 'easeInOut',
         overlay: false,
-        dotNavEnabled: true,
+        dotNavEnabled: false,
         // animationType: 'slideX',
       },
 
